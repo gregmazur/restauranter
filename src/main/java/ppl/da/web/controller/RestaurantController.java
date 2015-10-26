5680ffa6f489d877b8b8fa2acbdcdb9d16c957cb
gregmazur <grigoro.on.odessa@gmail.com>
2015-10-26 19:30:14 +0200

    controller works!

--- src/main/java/ppl/da/web/controller/RestaurantController.java
+++ src/main/java/ppl/da/web/controller/RestaurantController.java
@@ -1,12 +1,12 @@
 package ppl.da.web.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import ppl.da.core.entity.Point;
 import ppl.da.core.entity.Restaurant;
-import ppl.da.core.entity.User;
 import ppl.da.core.service.ServiceRestaurant;
 
 import java.util.List;
@@ -18,7 +18,9 @@ import java.util.List;
 @RequestMapping("/restaurant")
 public class RestaurantController {
 
+    private static final Logger logger = Logger.getLogger(RestaurantController.class);
     private ServiceRestaurant service;
+
     @Autowired
     public RestaurantController(ServiceRestaurant service) {
         this.service = service;
@@ -27,6 +29,7 @@ public class RestaurantController {
     @RequestMapping (method = RequestMethod.GET)
     public @ResponseBody List<Restaurant> getRestaurants(@RequestParam("lon")Double lon,@RequestParam("lat")Double lat,
                           @RequestParam("dis")Long dis){
+        logger.debug("getRestaurants started");
         List<Restaurant> restaurants;
         if(lon == null || lat == null){
             return null;
@@ -44,10 +47,18 @@ public class RestaurantController {
     }
     @RequestMapping (method = RequestMethod.POST, headers = "Accept=application/json")
     public @ResponseBody Restaurant createRestaurant(@RequestBody Restaurant restaurant){
+        logger.debug("createRestaurant started");
         return service.addRestaurant(restaurant);
     }
     @RequestMapping (value = "/add",method = RequestMethod.GET)
-    public String getRegistration(Model model){
+    public String getRegistration(){
+        logger.debug("getRegistration started");
         return "addrest";
+    }
+    @RequestMapping(value = "/test",method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+        logger.debug("test");
+        model.addAttribute("message", "Spring 3 MVC - Hello World");
+        return "hello";
     }
 }
